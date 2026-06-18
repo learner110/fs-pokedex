83f9801e9d50c116aed5ee0c58a1964bb11d5d6b
learner110 <amuhamed282@gmail.com>
2026-06-18 13:39:28 +0300

    korjailin

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
-
-// get the port from env variable
 const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 const start = async () => {
   await app.listen(PORT)
