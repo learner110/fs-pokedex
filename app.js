201ad3a8cae4d27779c7056c0b3f85e8a529d28a
learner110 <amuhamed282@gmail.com>
2026-06-23 13:00:31 +0300

    testailaan discord ilmoituksia

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('3')
+  res.send('1')
 })
 
 app.get('/health', (req, res) => {
